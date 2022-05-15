55ba80c95092ec4237ef8e419dbf9ebd20d4d08c
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-05-15 15:34:25 -0400

    Added resume section, Added hamburger menu at small screen sizes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Nav(props) {
     const {
@@ -6,23 +6,59 @@ function Nav(props) {
         currSection,
         setCurrSection
     } = props;
+    const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
     
     const changeSection = (sectionName) => {
         setCurrSection(sectionName);
     }
+
+    const toggleHamburger = () => {
+        setHamburgerIsOpen(!hamburgerIsOpen);
+    }
+
+    let [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', handleResize);
+    })
     
     return (
         <nav>
-            <ul>
-                {sections.map(section => (
-                    <li 
-                    className={section === currSection ? 'selected' : ''}
-                    key={section}
-                    onClick={() => changeSection(section)}>
-                        {section}
-                    </li>
-                ))}
-            </ul>
+                {width >= 1060 ? ( 
+                    <ul>
+                        {sections.map(section => (
+                            <li 
+                                className={section === currSection ? 'selected' : ''}
+                                key={section}
+                                onClick={() => changeSection(section)}>
+                                    {section}
+                            </li>))}
+                    </ul>
+                ) : (
+                    <>
+                        <svg onClick={() => toggleHamburger()} className='hamburger' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
+                        </svg>
+                        <div className='modal-container'>
+                            <ul className={hamburgerIsOpen ? 'modal open' : 'modal'}>
+                                {sections.map(section => (
+                                    <li 
+                                        className={section === currSection ? 'selected' : ''}
+                                        key={section}
+                                        onClick={() => {
+                                            changeSection(section);
+                                            toggleHamburger();
+                                        }}>
+                                            {section}
+                                    </li>))}
+                            </ul>
+                        </div>
+                    </>
+                )}
         </nav>
     )
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,8 +2,34 @@ import React from 'react';
 
 function Resume() {
     return (
-        <section>
-            Resume
+        <section className='resume-container'>
+            <div className='resume'>
+                <h2>View my resume <a href='https://www.linkedin.com/in/jonathan-mcdonnell-6a6a35156/overlay/1539471480117/single-media-viewer?type=DOCUMENT&profileId=ACoAACWHMJ4B4JxE7VKvBsM97Cxomx9pnnv89gw&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BXlAFSxCJSfi%2BMwJMBV6H4w%3D%3D'>here</a></h2>
+            </div>
+            <div>
+                <h2>Fron-End Proficiencies</h2>
+                <ul className='proficiencies'>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                    <li>HandleBars</li>
+                    <li>jQuery</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Back-End Proficiencies</h2>
+                <ul className='proficiencies'>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>SQL - MySQL</li>
+                    <li>NoSql - MongoDB</li>
+                    <li>Unit Testing - Jest</li>
+                    <li>APIs</li>
+                    <li>REST</li>
+                </ul>
+            </div>
         </section>
     )
 }
